260e5e2bb07d198ac1409e7ee1df4bbcb7fcc979
Alexandra <alexandra.riva@hyperisland.se>
2025-05-31 17:07:31 +0200

    Fixed spacing

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -7,8 +7,7 @@ export default function IntroSection() {
       <h1 className="intro-heading">
         Alexandra Riva<br />
         Front-end developer<br />
-        student<br />
-        at Hyper Island
+        student at Hyper Island.
       </h1>
     </section>
   );
